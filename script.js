87ca1736dd92b5911322767958bde53d8dcbb4ea
Andy Tran <foolmonkey99@gmail.com>
2020-05-21 11:06:54 -0600

    Add basic math operations in js

--- script.js
+++ script.js
@@ -1,15 +1,42 @@
-function add(){
-
+function add(firstNum, secondNum){
+    return firstNum + secondNum;
 }
 
-function subtract(){
-
+function subtract(firstNum, secondNum){
+    return firstNum - secondNum;
 }
 
-function multiply(){
-
+function multiply(firstNum, secondNum){
+    return firstNum * secondNum;
 }
 
-function operate(){
-    
+function divide(firstNum, secondNum){
+    return firstNum / secondNum;
 }
+
+function operate(firstNum, secondNum, operator){
+    let result;
+
+    switch(operator){
+        case '+':
+            result = add(firstNum, secondNum);
+            break;
+        case '-':
+            result = subtract(firstNum, secondNum);
+            break;
+        case '*':
+            result = multiply(firstNum, secondNum);
+            break;
+        case '/':
+            result = divide(firstNum, secondNum);
+            break;
+    }
+
+    return result;
+}
+
+
+console.log(operate(1, 2, '+'));
+console.log(operate(5, 2, '-'));
+console.log(operate(4, 8, '*'));
+console.log(operate(5, 10, '/'));
